4fce606039eda9281a89a20144b9c96ec25ead93
Paul Coch <34541870+paulbijancoch@users.noreply.github.com>
2020-12-30 17:26:05 +0100

    error handling

--- deleteTodo/index.js
+++ deleteTodo/index.js
@@ -7,16 +7,16 @@ const client = new CosmosClient({ endpoint, key });
 const container = client.database("dbtodos").container("items");
 
 module.exports = async function (context, req) {
-    const id = req.query.id;
+    const id = req.query.id || req.body.id;
     let res;
-    try{
+    try {
         res = await container.item(id).delete();
         context.res = {
             body: {'result': 'success'}
         };
-    }catch(err){
+    } catch(error) {
         context.res = {
             status: 400,
-            body: {'result': 'error'}
+            body: {'result': 'error', error}
         };
     }
