6cd81d3891193e5853c2bfc1541c2df6b2802bca
Paul Coch <34541870+paulbijancoch@users.noreply.github.com>
2020-12-30 17:28:32 +0100

    logging

--- deleteTodo/index.js
+++ deleteTodo/index.js
@@ -9,12 +9,15 @@ const container = client.database("dbtodos").container("items");
 module.exports = async function (context, req) {
     const id = req.query.id || req.body.id;
     let res;
+    console.log('reading ' + id);
     try {
         res = await container.item(id).delete();
+        console.log('delete ' + id);
         context.res = {
             body: {'result': 'success'}
         };
     } catch(error) {
+        console.error(error);
         context.res = {
             status: 400,
             body: {'result': 'error', error}
